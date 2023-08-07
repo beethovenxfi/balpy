6613fecd97952ae18c6163f039695877bd2b8f38
groninge <groninge@gmail.com>
2023-08-07 08:23:07 +0200

    use custom config

--- samples/poolVerification/poolVerification.py
+++ samples/poolVerification/poolVerification.py
@@ -7,14 +7,15 @@ import balpy
 
 def main():
 	network = "fantom"
-	poolId = "0x9FFF4C8A5AEB13B63219F39383A2C2689702F7CC000000000000000000000779"
+	customConfigFile = os.path.join('customConfig', 'fantom.json')
+	poolId = "0x6A488FA7DE0641511580AD833ADED393D64D6956000000000000000000000775"
 	# On Ethereum and Ethereum testnets, you can pass creationHash=None
 	# On Polygon, you must pass the pool creation hash to generate verification params
 	creationHash = None;
 	# creationHash = "0x18c7e1c9235c6e93878e55a87ed249f9d0ceb9d12ee584794e92f80f7645686d";
 	verbose = False;
 
-	bal = balpy.balpy.balpy(network);
+	bal = balpy.balpy.balpy(network, customConfigFile=customConfigFile);
 	isVerified = bal.isContractVerified(poolId, verbose=verbose);
 	if not isVerified:
 		command = bal.balGeneratePoolCreationArguments(poolId, verbose=verbose, creationHash=creationHash);
